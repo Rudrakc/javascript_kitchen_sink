f8848c44fad52ca47611dab591616c21a5c9dc99
Ayush Kesharwani <155665460+Ayush307K@users.noreply.github.com>
2024-09-26 21:04:33 +0530

    Valid move for bishop

--- public/javascript/chess/pieces/bishop.js
+++ public/javascript/chess/pieces/bishop.js
@@ -9,5 +9,21 @@ Bishop.prototype = new Piece({});
 Bishop.prototype.validateMove = function(targetPosition, board){
 
     // TODO: Ayush Kesharwani complete this functionality
+     // Convert current position to row and column
+    let currentCol = this.position.charAt(0);
+    let currentRow = parseInt(this.position.charAt(1));
+    
+    let targetCol = targetPosition.col;
+    let targetRow = parseInt(targetPosition.row);
+
+    // Check if the move is diagonal
+    if (Math.abs(targetCol.charCodeAt(0) - currentCol.charCodeAt(0)) === Math.abs(targetRow - currentRow)) {
+        // Diagonal movement is valid if row and column move distances are equal
+        return true;
+    }
+
+    // If the move is not diagonal, it's invalid for a bishop
+    console.warn("Invalid move for bishop");
+    return false;
 
 }
