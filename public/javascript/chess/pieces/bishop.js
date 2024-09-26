42026c867c5d4b0cf54485c09a1d90b599bbe927
Rudrakc <rudrachauhan2888@gmail.com>
2024-09-26 21:53:13 +0530

    Remove repeating function init in Bishop.js

--- public/javascript/chess/pieces/bishop.js
+++ public/javascript/chess/pieces/bishop.js
@@ -53,12 +53,3 @@ var Bishop = function(config) {
     this.type = 'bishop';
     this.constructor(config);
 };
-
-
-
-Bishop.prototype = new Piece({});
-Bishop.prototype.validateMove = function(targetPosition, board){
-
-    // TODO: Ayush Kesharwani complete this functionality
-
-}
