2fca437d01a58d08049af3d015a9d85a6561d5c6
Rudrakc <rudrachauhan2888@gmail.com>
2024-09-26 20:21:36 +0530

    Fixed Confetti issue

--- public/javascript/chess/pieces/queen.js
+++ public/javascript/chess/pieces/queen.js
@@ -1,75 +1,74 @@
-var Queen = function(config){
-    this.type = 'queen';
-    this.constructor(config);
+var Queen = function (config) {
+  this.type = "queen";
+  this.constructor(config);
+  this.position = config.position;
 };
 
 Queen.prototype = new Piece({});
 
-Queen.prototype.validateMove = function(targetPosition, board){
-    const curRow = this.position.charCodeAt(1) - 0;
-    const curCol = this.position.charCodeAt(0) - 64;
-    const tarRow = targetPosition.row.charCodeAt(0) - 0;
-    const tarCol = targetPosition.col.charCodeAt(0) - 64;
+Queen.prototype.validateMove = function (targetPosition, board) {
+  const curRow = this.position.charCodeAt(1) - 0;
+  const curCol = this.position.charCodeAt(0) - 64;
+  const tarRow = targetPosition.row.charCodeAt(0) - 0;
+  const tarCol = targetPosition.col.charCodeAt(0) - 64;
 
-    const rowDiff = Math.abs(tarRow - curRow);
-    const colDiff = Math.abs(tarCol - curCol);
+  const rowDiff = Math.abs(tarRow - curRow);
+  const colDiff = Math.abs(tarCol - curCol);
 
+  if (curRow === tarRow || curCol === tarCol) {
+    return this.isPathClearStraight(curRow, curCol, tarRow, tarCol, board);
+  }
 
-    if (rowDiff === 0 || colDiff === 0) {
-        if (!this.isPathClear(curRow, curCol, tarRow, tarCol, board)) {
-            return false;
-        }
-        return true;
-    }
+  if (rowDiff === colDiff) {
+    return this.isPathClearDiagonal(curRow, curCol, tarRow, tarCol, board);
+  }
 
-
-    if (rowDiff === colDiff) {
-        if (!this.isDiagonalPathClear(curRow, curCol, tarRow, tarCol, board)) {
-            return false;
-        }
-        return true;
-    }
-
-
-    return false;
+  return false;
 };
 
-Queen.prototype.isPathClear = function(curRow, curCol, tarRow, tarCol, board) {
-
-    if (curRow === tarRow) {
-        const step = curCol < tarCol ? 1 : -1;
-        for (let col = curCol + step; col !== tarCol; col += step) {
-            if (board.getPieceAt({row: String.fromCharCode(0 + curRow), col: String.fromCharCode(64 + col)})) {
-                return false;
-            }
-        }
-    } else if (curCol === tarCol) {
-        const step = curRow < tarRow ? 1 : -1;
-        for (let row = curRow + step; row !== tarRow; row += step) {
-            if (board.getPieceAt({row: String.fromCharCode(0 + row), col: String.fromCharCode(64 + curCol)})) {
-                return false;
-            }
-        }
+Queen.prototype.isPathClearStraight = function (
+  curRow,
+  curCol,
+  tarRow,
+  tarCol,
+  board
+) {
+  if (curRow === tarRow) {
+    const step = curCol < tarCol ? 1 : -1;
+    for (let col = curCol + step; col !== tarCol; col += step) {
+      if (board.getPieceAt(curRow, String.fromCharCode(col))) {
+        return false;
+      }
     }
-    return true;
+  } else {
+    const step = curRow < tarRow ? 1 : -1;
+    for (let row = curRow + step; row !== tarRow; row += step) {
+      if (board.getPieceAt(row, String.fromCharCode(curCol))) {
+        return false;
+      }
+    }
+  }
+  return true;
 };
 
-Queen.prototype.isDiagonalPathClear = function(curRow, curCol, tarRow, tarCol, board) {
+Queen.prototype.isPathClearDiagonal = function (
+  curRow,
+  curCol,
+  tarRow,
+  tarCol,
+  board
+) {
+  const rowStep = curRow < tarRow ? 1 : -1;
+  const colStep = curCol < tarCol ? 1 : -1;
+  let row = curRow + rowStep;
+  let col = curCol + colStep;
 
-    const rowStep = curRow < tarRow ? 1 : -1;
-    const colStep = curCol < tarCol ? 1 : -1;
-    let row = curRow + rowStep;
-    let col = curCol + colStep;
-
-    while (row !== tarRow && col !== tarCol) {
-        if (board.getPieceAt({
-            row: String.fromCharCode(0 + row),
-            col: String.fromCharCode(64 + col)
-        })) {
-            return false;
-        }
-        row += rowStep;
-        col += colStep;
+  while (row !== tarRow && col !== tarCol) {
+    if (board.getPieceAt(row, String.fromCharCode(col))) {
+      return false;
     }
-    return true;
+    row += rowStep;
+    col += colStep;
+  }
+  return true;
 };
