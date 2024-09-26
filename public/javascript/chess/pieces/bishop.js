2f55d018b9b90d2bc3fc763d58ea5a150027bec0
Ayush307K <ayush.23bcs10112@ms.sst.scaler.com>
2024-09-26 21:41:19 +0530

    Added bishop logic

--- public/javascript/chess/pieces/bishop.js
+++ public/javascript/chess/pieces/bishop.js
@@ -1,4 +1,55 @@
-var Bishop = function(config){
+var Bishop = function(config) {
+    this.type = "bishop";
+    this.constructor(config);
+  };
+  
+  Bishop.prototype = new Piece({});
+  
+  Bishop.prototype.validateMove = function(targetPosition, board) {
+    const curRow = this.position.charCodeAt(1) - 0;
+    const curCol = this.position.charCodeAt(0) - 64;
+    const tarRow = targetPosition.row.charCodeAt(0) - 0;
+    const tarCol = targetPosition.col.charCodeAt(0) - 64;
+  
+    const rowDiff = Math.abs(tarRow - curRow);
+    const colDiff = Math.abs(tarCol - curCol);
+  
+    // Bishop moves diagonally, which means rowDiff must be equal to colDiff
+    if (rowDiff !== colDiff) {
+      return false;
+    }
+  
+    if (!this.isPathClear(curRow, curCol, tarRow, tarCol, board)) {
+      return false;
+    }
+  
+    return true;
+  };
+  
+  Bishop.prototype.isPathClear = function(curRow, curCol, tarRow, tarCol, board) {
+    const rowStep = curRow < tarRow ? 1 : -1;
+    const colStep = curCol < tarCol ? 1 : -1;
+  
+    let row = curRow + rowStep;
+    let col = curCol + colStep;
+  
+    while (row !== tarRow && col !== tarCol) {
+      if (
+        board.getPieceAt({
+          row: String.fromCharCode(0 + row),
+          col: String.fromCharCode(64 + col),
+        })
+      ) {
+        return false;
+      }
+  
+      row += rowStep;
+      col += colStep;
+    }
+  
+    return true;
+  };
+  var Bishop = function(config){
     this.type = 'bishop';
     this.constructor(config);
 };
